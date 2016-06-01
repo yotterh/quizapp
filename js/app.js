71f8ba0824c7fe8fb704765703d1d17c0f4f9ce9
yotterh <hollyyotter11@gmail.com>
2016-05-31 20:43:21 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
-var quiz = [
+	var quiz = [
 	{
 		number: 1, 
 		image: "http://www.tajmahal.org.uk/gifs/taj-mahal.jpeg",
-    	title: "Where is the Taj Mahal located?", 
+		title: "Where is the Taj Mahal located?", 
 		choices: ["Istanbul, Turkey", "Marrakesh, Morocco", "Agra, India", "Cairo, Egypt"
 		],
 		answer: 3,
@@ -14,7 +14,7 @@ var quiz = [
 	{
 		number: 2, 
 		image: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Dean_Franklin_-_06.04.03_Mount_Rushmore_Monument_(by-sa)-3_new.jpg",
-    	title: "Where is Mount Rushmore located?", 
+		title: "Where is Mount Rushmore located?", 
 		choices: ["Bismark, North Dakota", "Keystone, South Dakota", "Billings, Montana", "Rapid City, South Dakota"
 		],
 		answer: 2,
@@ -24,7 +24,7 @@ var quiz = [
 	{
 		number: 3,
 		image: "http://www.chinatourguide.com/china_photos/beijing/attractions/Beijing_Forbidden_City_taihe_palace.jpg", 
-    	title: "Where is the Forbidden City located?", 
+		title: "Where is the Forbidden City located?", 
 		choices: ["Beijing, China", "Shanghai, China", "Hong Kong, China", "Guangzhou, China"
 		],
 		answer: 1,
@@ -34,7 +34,7 @@ var quiz = [
 	{
 		number: 4, 
 		image: "http://static.thousandwonders.net/Christ.the.Redeemer.original.2544.jpg",
-    	title: "Where is Christ the Redeemer located?", 
+		title: "Where is Christ the Redeemer located?", 
 		choices: ["Buenos Aires, Argentina", "La Paz, Bolivia", "Lima, Peru", "Rio de Janeiro, Brazil",
 		],
 		answer: 4, 
@@ -44,55 +44,56 @@ var quiz = [
 	{
 		number: 5,
 		image: "http://www.funcage.com/blog/wp-content/uploads/2014/03/The-Acropolis.jpg", 
-    	title: "Where is Acropolis located?", 
+		title: "Where is Acropolis located?", 
 		choices: ["Naples, Italy", "Barcelona, Spain", "Athens, Greece", "Budapest, Hungary"
 		],
 		answer: 3,
 		answertext: "The correct answer is Athens, Greece", 
 		userAnswer: null
 	}
-];
+	];
 
-var currentQuestion = 1;
-var correctAnswers = 0;
-var totalQuestions = quiz.length;
-var questionIndex = 0;
-var correctText = "Correct!";
-var wrongText = "That's incorrect";
+	var currentQuestion = 1;
+	var correctAnswers = 0;
+	var totalQuestions = quiz.length;
+	var questionIndex = 0;
+	var correctText = "Correct!";
+	var wrongText = "That's incorrect";
 	
-  $("#start").click(function() {
-    $(".begintext").hide();
-    $("#quiz").show();
-    $("#Next").hide();
-    $("#Submit").show();
-    getQuestion();
-  });
+	$("#start").click(function() {
+		$(".begintext").hide();
+		$("#quiz").show();
+		$("#Next").hide();
+		$("#Submit").show();
+		getQuestion();
+	});
 
-function startGame() {
-	  	currentQuestion = 1;
+	function startGame() {
+		currentQuestion = 1;
 		correctAnswers = 0;
 		questionIndex = 0;
 		$("#quiz").show();
 		$("#Next").hide();
-   		 $("#Submit").show();
-		$("#playAgain").hide();
+		$("#Submit").show();
+		$("#playagain").hide();
+		$("#finalscore").hide();
 		$('input:radio[name=value]').attr('checked',false);
-	  getQuestion();
- };
- function getQuestion() {
-  $(".landmark").html('<img src="' + quiz[questionIndex].image + '"/>');	
-  $("#question1").text(quiz[questionIndex].title);
-  $("#option1").text(quiz[questionIndex].choices[0]);
-  $("#option2").text(quiz[questionIndex].choices[1]);
-  $("#option3").text(quiz[questionIndex].choices[2]);
-  $("#option4").text(quiz[questionIndex].choices[3]);
- };
+		getQuestion();
+	};
+	function getQuestion() {
+		$(".landmark").html('<img src="' + quiz[questionIndex].image + '"/>');	
+		$("#question1").text(quiz[questionIndex].title);
+		$("#option1").text(quiz[questionIndex].choices[0]);
+		$("#option2").text(quiz[questionIndex].choices[1]);
+		$("#option3").text(quiz[questionIndex].choices[2]);
+		$("#option4").text(quiz[questionIndex].choices[3]);
+	};
 
-$("#Submit").click(function() {	
-			checkAnswer();
-});
+	$("#Submit").click(function() {	
+		checkAnswer();
+	});
 
- 	function checkAnswer() {
+	function checkAnswer() {
 		var radioValue = false;
 		var userChoice = document.getElementsByName('value');
 		for (var i = 0; i < userChoice.length; i++) {
@@ -116,7 +117,7 @@ $("#Submit").click(function() {
 			$("#correctlocation").show()
 			$("#correctlocation").text(quiz[questionIndex].answertext);
 			$("#Submit").hide();
-		
+
 		};
 		if (quiz[questionIndex].number == 5) {
 
@@ -124,45 +125,43 @@ $("#Submit").click(function() {
 				$("#correctlocation").html('<h2>' + correctText + '</h2>');
 				$("#correctlocation").show();
 
-				} else {
+			} else {
 				$("#correctlocation").show()
 				$("#correctlocation").text(quiz[questionIndex].answertext);
-				}
+			}
 
-				$("#finalscore").text("Congrats! You got " + correctAnswers + " out of 5 correct!");
+			$("#finalscore").text("Congrats! You got " + correctAnswers + " out of 5 correct!");
 			
-				$("#playagain").show();
+			$("#playagain").show();
 			
-				$("#Submit").hide();
+			$("#Submit").hide();
 
 		} else {
-				$("#Next").show();
+			$("#Next").show();
 		};
-
-		
+	}; 
 
 	$("#Next").click(function() {
-			nextQuestion();
-			$('input:radio[name=value]').attr('checked',false);
-		});
+		nextQuestion();
+		$('input:radio[name=value]').attr('checked',false);
+	});
 
-function nextQuestion() {
-	$("#Next").hide();
-	$("#Submit").show();
-	$("#correctlocation").hide();
-	currentQuestion++;
-	questionIndex++;
-	getQuestion();
+	function nextQuestion() {
+		$("#Next").hide();
+		$("#Submit").show();
+		$("#correctlocation").hide();
+		currentQuestion++;
+		questionIndex++;
+		getQuestion();
 
-}; 
-
-		$("#playagain").click(function() {
-			$("#Submit").show();
-			$("#Next").hide();
-			$("#correctlocation").hide();
-			startGame();	
-		});
 	}; 
+
+	$("#playagain").click(function() {
+		$("#Submit").show();
+		$("#Next").hide();
+		$("#correctlocation").hide();
+		startGame();	
+	});
 });
 
 
